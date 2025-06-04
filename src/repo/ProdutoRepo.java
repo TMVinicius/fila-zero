65ed5569a3b218be3708c5d224edbb169df06c70
TMVinicius <viniciustavaresbr@gmail.com>
2025-06-03 22:13:41 -0300

    Inserindo metodos de busca e salvamento de produto

--- src/repo/ProdutoRepo.java
+++ src/repo/ProdutoRepo.java
@@ -1,21 +1,19 @@
 package repo;
 
 import model.Produto;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ProdutoRepo {
 
     public void salvar(Produto produto) {
-        String sql = "INSERT INTO produto (nomeProduto, codigoDeBarras, precoUnitario) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO produto (nomeProduto, codigoDeBarras, precoUnitario, quantidade, idEstabelecimento) VALUES (?, ?, ?, ?, ?)";
 
         try (Connection conn = Conexao.conectar(); PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setString(1, produto.getNomeProduto());
             stmt.setInt(2, produto.getCodigoDeBarras());
             stmt.setDouble(3, produto.getPrecoUnitario());
+            stmt.setInt(4, produto.getQuantidade());
+            stmt.setInt(5, produto.getEstabelecimento().getId());
 
             stmt.executeUpdate();
         } catch (SQLException e) {
@@ -33,12 +31,12 @@ public class ProdutoRepo {
             ResultSet rs = stmt.executeQuery();
 
             if (rs.next()) {
-                produto = new Produto(
-                    rs.getInt("id"),
-                    rs.getString("nomeProduto"),
-                    rs.getInt("codigoDeBarras"),
-                    rs.getDouble("precoUnitario")
-                );
+                produto = new Produto();
+                produto.setIdProduto(rs.getInt("id"));
+                produto.setNomeProduto(rs.getString("nomeProduto"));
+                produto.setCodigoDeBarras(rs.getInt("codigoDeBarras"));
+                produto.setPrecoUnitario(rs.getDouble("precoUnitario"));
+                produto.setQuantidade(rs.getInt("quantidade"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
